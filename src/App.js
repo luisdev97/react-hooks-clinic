e80fe76e17c75ea3b2e031e92485e7b7dd2c082d
luisdev97 <amadormontorodavid@gmail.com>
2019-12-05 23:58:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Form from './components/Form';
+
+
+const App = () => {
+
+  const [ appoinments , setAppoinments ] = useState(); 
+
+  return (
+    <>
+      <h1>Patient manager</h1>
+      <div className="container">
+        <div className="row">
+
+          <div className="one-half column">
+
+          </div>
+
+          <div className="one-half column">
+
+          </div>
+
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default App;
